1f758e48c8fbb478cc001398090226a06975f772
Your Name <you@example.com>
2024-07-24 16:56:44 +0530

    client side link changed

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
       return;
     }
 
-    // Construct the URL with query parameters
+    // Construct the URL with query parameters https://bo-cilent-updated.vercel.app/
     // const message = `${domain}/${clientId}/${month}/${year}`;
     // const iframeUrl = `http://localhost:${port}/?message=${encodeURIComponent(message)}`;
-    const url = `https://bo-cilent-a5xb.vercel.app/?domain=${encodeURIComponent(domain)}&clientId=${encodeURIComponent(clientId)}&month=${encodeURIComponent(month)}&year=${encodeURIComponent(year)}`
+    const url = `https://bo-cilent-updated.vercel.app/?domain=${encodeURIComponent(domain)}&clientId=${encodeURIComponent(clientId)}&month=${encodeURIComponent(month)}&year=${encodeURIComponent(year)}`
     // Create the iframe
     const iframe = document.createElement("iframe");
     console.log('iframe =', iframe);
